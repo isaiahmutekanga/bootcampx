1edcd54f9a6b441ee79342b32cf10c815921edec
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-22 22:07:39 +0000

    JOIN

--- 1_queries/gmail.sql
+++ 1_queries/gmail.sql
@@ -2,3 +2,5 @@ SELECT name, id, email, cohort_id
 FROM students
 WHERE email NOT LIKE '%gmail.com'
 AND phone IS NULL;
+
+SELECT students.name , students.start_date,cohorts.name FROM students JOIN cohorts ON cohorts.id = cohort_id;
